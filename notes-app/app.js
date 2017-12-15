73bec8e5e1a4b8b15db8ba36b25017b12a3a663b
Cloud <cloud@kevia.me>
2017-12-14 20:59:13 -0700

    testing stuff with lodash

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,12 +2,14 @@ console.log('Starting application')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js')
 
 let user = os.userInfo()
 let sum = notes.add(5, 5)
 
 console.log(sum)
+console.log(_.isString(true))
 
 fs.appendFile('helloworld.txt', `Hello ${user.username}`, (err) => {
   if (err) console.log(`Error: ${err}`)
