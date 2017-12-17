b1bebba29d595469f35990d30972cd11d424dcd8
Cloud <cloud@kevia.me>
2017-12-16 20:49:25 -0700

    pulling in argv and chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,3 +2,18 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const notes = require('./notes.js')
+
+let command = (process.argv[2] === undefined) ? null :  process.argv[2].toLowerCase()
+console.log(`Command is: ${command}`)
+
+if (command === 'add') {
+  console.log('adding to list')
+} else if (command === 'list') {
+  console.log('displaying list')
+} else if (command === 'read') {
+  console.log('read what')
+} else if (command === 'remove') {
+  console.log('remove what')
+} else {
+  console.warn(`Command: ${command} is unrecognizable`)
+}
