66d97400fb63bf7aadc1d49266e1d8ca2592df45
Cloud <cloud@kevia.me>
2017-12-20 19:53:40 -0700

    adding HMR to create-react-app

--- learn-router/src/index.js
+++ learn-router/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const HMR = require('./App').default
+    ReactDOM.render(
+      <HMR />,
+      document.getElementById('root')
+    )
+  })
+}
+
+registerServiceWorker()
