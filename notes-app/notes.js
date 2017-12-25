61b06809535bba12cebf84a63a71ab54962cbb90
Cloud <cloud@kevia.me>
2017-12-25 10:42:16 -0700

    reading and adding

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,13 +2,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 const addNote = (title, body) => {
-  let notes = []
   let note = {
     title,
     body
   }
 
+  let noteString = fs.readFileSync('notes-data.json')
+  let notes = JSON.parse(noteString)
   let updatedNotes = notes.concat(note)
+  
   fs.writeFileSync('notes-data.json', JSON.stringify(updatedNotes))
 }
 
