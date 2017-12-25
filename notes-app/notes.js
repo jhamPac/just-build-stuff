c9674d0af77d46244ccf94a848f1527fbb8f5141
Cloud <cloud@kevia.me>
2017-12-25 10:56:37 -0700

    using const for filename

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
+const NOTES_FILE_NAME = 'notes-data.json'
+
 const addNote = (title, body) => {
   let newNote = {
     title,
     body
   }
 
-  let notes = _getNotes('notes-data.json')
+  let notes = _getNotes(NOTES_FILE_NAME)
   let updatedNotes = notes.concat(newNote)
 
-  fs.writeFileSync('notes-data.json', JSON.stringify(updatedNotes))
+  fs.writeFileSync(NOTES_FILE_NAME, JSON.stringify(updatedNotes))
 }
 
 _getNotes = (fileName) => {
