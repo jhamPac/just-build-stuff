e8abce1adfc600bd14a4be559cfe28ed144da005
Cloud <cloud@kevia.me>
2017-12-25 10:36:57 -0700

    test writeFileSync

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,15 @@
+const fs = require('fs')
 const chalk = require('chalk')
 
 const addNote = (title, body) => {
-  console.log(chalk.yellow(title, body))
+  let notes = []
+  let note = {
+    title,
+    body
+  }
+
+  let updatedNotes = notes.concat(note)
+  fs.writeFileSync('notes-data.json', JSON.stringify(updatedNotes))
 }
 
 module.exports = {
