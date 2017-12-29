8b8561d36276f6d97e5427258b3cdbb60aad1d2c
Cloud <cloud@kevia.me>
2017-12-29 11:49:08 -0700

    added a docker-compose.yml for services

--- dockerize/app.js
+++ dockerize/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  response.send('Hello World')
+  response.send('Hello World this is great')
 })
 
 app.listen(3000)
+console.log('App is listening on port 3000')
